3170827b684427785c672ee9da7bc7ae4fc18645
emotionalproof <chaserabenn@gmail.com>
2020-05-19 00:37:11 -0400

    Done.

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,3 +1,4 @@
+require 'pry'
 
 def start_game(player1, player2)
   "Hello #{player1} & #{player2}"
@@ -6,3 +7,5 @@ end
 def play_game(player1, player2)
   "#{player1} is better than #{player2}"
 end
+
+binding.pry
